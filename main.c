62c75fcc8e2d7e43d4fb714e353aa714c1af3cb3
Yash Karthik <52369876+YashKarthik@users.noreply.github.com>
2023-06-06 17:37:00 +0530

    func

--- main.c
+++ main.c
@@ -1,10 +1,16 @@
 #include <stdio.h>
 
+int power(int m, int n) {
+  int result = 1;
+  for (int i = 0; i < n; i++) {
+    result *= m;
+  }
+  return result;
+}
+
 int main() {
-  int newlines = 0;
-  int c;
-  while ((c = getchar()) != EOF)
-    if (c == '\n')
-      ++newlines;
-  printf("\nCount: %d\n", newlines);
+  int r = power(3, 2);
+  printf("Power 2^2: %d\n", r);
+
+  return 0;
 }
